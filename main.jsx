44b235ef52383dba7f519129e3b027284af13e4c
MarianC312 <visual_design@live.com.ar>
2020-08-02 18:28:07 -0300

    Simple commit

---
+++ main.jsx
@@ -0,0 +1,41 @@
+class Main extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            bg: "gray"
+        };
+        this.colorUp = this.colorUp.bind(this);
+        this.randMeUp = this.randMeUp.bind(this);
+    }
+    
+    colorUp(){
+        let newColor = "#";
+        do{
+            newColor += this.randMeUp();
+        }while(newColor.length < 7);
+        console.log(newColor);
+        this.setState({
+            bg: newColor
+        })
+    }
+    
+    randMeUp(){
+        const base = Math.floor(Math.random() * 10) % 2;
+        if(base == 0){
+            return String.fromCharCode(Math.floor(Math.random() * 6) + 65);
+        }else{
+            return Math.floor(Math.random() * 9);
+        }
+    }
+    
+    render(){
+        const color = this.state.bg;
+        return(
+            <div id="bg-rand" style={{backgroundColor: color}}>
+                <button type="button" onClick={this.colorUp} class="btn btn-primary">Change!</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById("body"));
